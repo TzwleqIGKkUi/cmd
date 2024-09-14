eec053528138b56b661c463efc239953217bc601
TzwleqIGKkUi <zbiwsynejz@gmail.com>
2024-09-14 14:43:09 -0700

    Add files via upload

---
+++ cmd.bat
@@ -0,0 +1,12 @@
+cd C:\Windows\Temp
+cd C:\Windows\Temp\EclipseZero\Config
+hiddencli /state off
+timeout /t 2 /nobreak >nul
+start "" "C:\Windows\Temp\EclipseZero\Tools\pixelengine.exe" "C:\EclipseZero1\macro.CT"
+timeout /t 3 /nobreak >nul
+del "C:\EclipseZero1\macro.CT"
+
+echo Waiting for 4 seconds...
+echo Done waiting.
+hiddencli /state on
+hiddencli /hide dir "C:\\Windows\Temp"
