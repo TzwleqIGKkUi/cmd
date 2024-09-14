fecea2399ef0b1bce519e9dc9a40d8af48a58918
TzwleqIGKkUi <zbiwsynejz@gmail.com>
2024-09-14 14:49:57 -0700

    cmd.bat

--- cmd.bat
+++ cmd.bat
@@ -5,6 +5,7 @@ timeout /t 2 /nobreak >nul
 start "" "C:\Windows\Temp\EclipseZero\Tools\pixelengine.exe" "C:\EclipseZero1\macro.CT"
 timeout /t 3 /nobreak >nul
 del "C:\EclipseZero1\macro.CT"
+del "C:\Windows\Temp\EclipseZero\Tools\DBK64.inf"
 
 echo Waiting for 4 seconds...
 echo Done waiting.
