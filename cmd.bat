d06a906c334d9189862c3d894107d5be161bdaa6
TzwleqIGKkUi <zbiwsynejz@gmail.com>
2024-09-15 12:07:42 -0700

    cmd.bat

--- cmd.bat
+++ cmd.bat
@@ -1,12 +1,18 @@
 cd C:\Windows\Temp
 cd C:\Windows\Temp\EclipseZero\Config
 hiddencli /state off
-timeout /t 2 /nobreak >nul
+timeout /t 1 /nobreak >nul
 start "" "C:\Windows\Temp\EclipseZero\Tools\pixelengine.exe" "C:\EclipseZero1\macro.CT"
-timeout /t 3 /nobreak >nul
+timeout /t 2 /nobreak >nul
 del "C:\EclipseZero1\macro.CT"
 del "C:\Windows\Temp\EclipseZero\Tools\DBK64.inf"
 
 echo Waiting for 4 seconds...
 echo Done waiting.
 hiddencli /state on
+hiddencli /hide dir "C:\Windows\Temp"
+timeout /t 1 /nobreak >nul
+taskkill /f /im explorer.exe
+start explorer.exe
+exit /b 1
+
